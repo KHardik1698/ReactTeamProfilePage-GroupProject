c1b876b804e5b5c3b12cf0dcb8d82a1c4281af56
panditaditi07 <aditi.pandit@raweng.com>
2020-12-29 16:19:17 +0530

    added styles to the profile page
    added readme file

--- frontend/src/components/ProfilePage.jsx
+++ frontend/src/components/ProfilePage.jsx
@@ -1,16 +1,28 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
+import styles from "./ProfilePage.module.css";
 
 class ProfilePage extends Component {
   render() {
+    console.log(this.props);
     let currentEmployee = this.props.location.state.employee;
+
     return (
-      <div>
-        <h1>{currentEmployee.name}</h1>
-        <img src={currentEmployee.imageUrl} alt="" />
-        <h2>{currentEmployee.company}</h2>
-        <h4>{currentEmployee.designation}</h4>
-        <p>{currentEmployee.description}</p>
+      <div className={styles["container"]}>
+        <img
+          src={currentEmployee.imageUrl}
+          className={styles["image-container"]}
+          alt=""
+        />
+        <div className={styles["employee-info"]}>
+          <h6 className={styles["company"]}>
+            <i>{currentEmployee.company}</i>
+          </h6>
+          <p className={styles["designation"]}>{currentEmployee.designation}</p>
+          <p className={styles["name"]}>{currentEmployee.name}</p>
+
+          <p className={styles["description"]}>{currentEmployee.description}</p>
+        </div>
       </div>
     );
   }
