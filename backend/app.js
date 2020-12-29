1165e9f13788d8969c310007fcc20b38278546ca
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-29 11:38:33 +0530

    Added Cors dependency to backend express server
    Cors will be used to integrate the backend server with frontend requests

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors");
 const empRoute = require("./routes/empRoutes.js");
 dotenv.config({
-	path: `${__dirname}/config.env`,
+  path: `${__dirname}/config.env`,
 });
 const app = express();
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/employees", empRoute);
 app.listen(process.env.PORT, () => {
-	console.log(`Server started at PORT ${process.env.PORT}`);
+  console.log(`Server started at PORT ${process.env.PORT}`);
 });
