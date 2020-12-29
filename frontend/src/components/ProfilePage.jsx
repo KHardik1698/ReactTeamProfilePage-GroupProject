30d241425640cb33c13c5534d363dec8a23b7a11
Ashwini-Rathod <ashwini.rathod@contentstack.com>
2020-12-29 16:30:12 +0530

    Conditional rendering implemented for invalid Url

--- frontend/src/components/ProfilePage.jsx
+++ frontend/src/components/ProfilePage.jsx
@@ -1,17 +1,45 @@
 import { Component } from "react";
 import { withRouter } from "react-router-dom";
-
+const url = "http://localhost:5000/employees";
 class ProfilePage extends Component {
+  state={
+    employee : [],
+    status: "",
+  }
+
+  componentDidMount = ()=>{
+    fetch(`${url}/${this.props.match.params.id}`)
+    .then((response)=>{
+      return response.json();
+    })
+    .then((data)=>{
+      console.log(data);
+      this.setState({employee: data.data, status: data.status});
+
+    })
+    .catch((err)=>{
+      console.log(err);
+    })
+  }
   render() {
-    let currentEmployee = this.props.location.state.employee;
+    console.log(this.state.employee);
+    console.log(this.state.status);
     return (
       <div>
-        <h1>{currentEmployee.name}</h1>
-        <img src={currentEmployee.imageUrl} alt="" />
-        <h2>{currentEmployee.company}</h2>
-        <h4>{currentEmployee.designation}</h4>
-        <p>{currentEmployee.description}</p>
+        {
+          this.state.status === "Successful"? (
+            <div>
+            <h1>{this.state.employee.name}</h1>
+            <img src={this.state.employee.imageUrl} alt="" />
+            <h2>{this.state.employee.company}</h2>
+            <h4>{this.state.employee.designation}</h4>
+            <p>{this.state.employee.description}</p>
+          </div>
+          ) :
+          <h1>Not found</h1>
+        }
       </div>
+
     );
   }
 }
