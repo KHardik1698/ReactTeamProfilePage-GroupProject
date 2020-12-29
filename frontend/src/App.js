11dd4fe7afd7a56cb9d4c73bddd9e7d28a9ad61f
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-29 11:41:58 +0530

    Added functio to fetch the data from backend
    Rendered Employee Tiles using data from backend server using React Routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,9 @@
 import "./App.css";
-import ProfilePage from "./components/ProfilePage";
 
 function App() {
   return (
     <div className="App">
       <h1>Main Page</h1>
-      <ProfilePage />
     </div>
   );
 }
