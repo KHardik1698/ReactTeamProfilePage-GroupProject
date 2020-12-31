5736886ce601055586aebd7aeedbc0435c6e472a
KHardik1698 <hardik.kundalia@contentstack.com>
2020-12-31 12:16:11 +0530

    Added Loading to Home Page & Profile Page
    Removed Header & Footer from Page Not Found Component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,9 @@
 import { Component } from "react";
 import { Link } from "react-router-dom";
 import styles from "./Home.module.css";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import initFontAwesome from "../components/initFontAwesome";
 const url = "https://employee-card-backend.herokuapp.com/employees";
 
 class Home extends Component {
@@ -8,6 +11,7 @@ class Home extends Component {
     super(props);
     this.state = {
       employees: [],
+      status: "",
     };
   }
   componentDidMount = (event) => {
@@ -16,15 +20,17 @@ class Home extends Component {
         return response.json();
       })
       .then((data) => {
-        this.setState({ employees: data.data });
+        this.setState({ employees: data.data, status: data.status });
       })
       .catch((err) => {
         console.log(err);
       });
   };
   render() {
+    initFontAwesome();
     return (
       <div>
+        <Navigation />
         <div className={styles["employee-header"]}>
           <p>Our Team</p>
         </div>
@@ -32,43 +38,50 @@ class Home extends Component {
           <p>Trainee Batch 2020 - Raw Enigneering/Contentstack </p>
         </div>
         <div className={styles["employee-container"]}>
-          {this.state.employees.map((employee) => {
-            return (
-              <div key={employee.id}>
-                <Link
-                  to={`/employees/${employee.id}`}
-                  className={styles["employee-card-container"]}
-                >
-                  <div className={styles["employee-card"]}>
-                    <div className={styles["image-div"]}>
-                      <img
-                        className={styles["employee-image"]}
-                        src={employee.imageUrl}
-                        alt={`${employee.name}-profilePicture`}
-                      />
+          {this.state.status === "" ? (
+            <div className={styles["loading"]}>
+              <h1>Loading...</h1>
+            </div>
+          ) : (
+            this.state.employees.map((employee) => {
+              return (
+                <div key={employee.id}>
+                  <Link
+                    to={`/employees/${employee.id}`}
+                    className={styles["employee-card-container"]}
+                  >
+                    <div className={styles["employee-card"]}>
+                      <div className={styles["image-div"]}>
+                        <img
+                          className={styles["employee-image"]}
+                          src={employee.imageUrl}
+                          alt={`${employee.name}-profilePicture`}
+                        />
+                      </div>
+                      <div className={styles["employee-details"]}>
+                        <div className={styles["employee-name"]}>
+                          <p> {employee.name} </p>
+                        </div>
+                        <div className={styles["employee-company"]}>
+                          <p> {employee.company} </p>
+                        </div>
+                        <div className={styles["employee-description"]}>
+                          <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Quod, ratione fugiat aliquid nesciunt deleniti
+                            dolor earum quidem eos nihil odio ea voluptas magnam
+                            libero accusamus exercitationem vitae ex, nam omnis?
+                          </p>
+                        </div>
+                      </div>
                     </div>
-                    <div className={styles["employee-details"]}>
-                      <div className={styles["employee-name"]}>
-                        <p> {employee.name} </p>
-                      </div>
-                      <div className={styles["employee-company"]}>
-                        <p> {employee.company} </p>
-                      </div>
-                      <div className={styles["employee-description"]}>
-                        <p>
-                          Lorem ipsum dolor sit amet consectetur adipisicing
-                          elit. Quod, ratione fugiat aliquid nesciunt deleniti
-                          dolor earum quidem eos nihil odio ea voluptas magnam
-                          libero accusamus exercitationem vitae ex, nam omnis?
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              </div>
-            );
-          })}
+                  </Link>
+                </div>
+              );
+            })
+          )}
         </div>
+        <Footer />
       </div>
     );
   }
--- frontend/src/routers/routers.js
+++ frontend/src/routers/routers.js
@@ -1,19 +1,14 @@
 import { Component } from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
-import Navigation from "../components/Navigation";
 import Home from "../pages/Home";
 import ProfilePage from "../pages/ProfilePage";
-import Footer from "../components/Footer";
 import NotFound from "../pages/NotFound";
-import initFontAwesome from "../components/initFontAwesome";
 
 class Router extends Component {
   render() {
-    initFontAwesome();
     return (
       <div>
         <BrowserRouter>
-          <Navigation />
           <Switch>
             <Route path="/" exact component={Home} />
             <Route
@@ -25,7 +20,6 @@ class Router extends Component {
             />
             <Route component={NotFound} />
           </Switch>
-          <Footer />
         </BrowserRouter>
       </div>
     );
